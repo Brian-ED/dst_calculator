6d265d2e533f2db4670ab874cd8fe76062e60ea8
grumm9690 <91623040+grumm9690@users.noreply.github.com>
2021-12-09 18:04:53 +0300

    Improvements
    A lot of improvements and fixes

--- insulation_calculator.py
+++ insulation_calculator.py
@@ -5,16 +5,19 @@ dress_dict = { "Dapper Vest" : 60, "Rain Coat" : 60, "Hibernation Vest" : 240, "
                "None" : 0}
 beard_dict = { "Wilson (Tier 3 Beard)" : 135, "Webber (Tier 3 Beard)" : 67.5, "Wilson (Tier 2 Beard)" : 45,
                "Webber (Tier 2 Beard)" : 33.75, "Wilson (Tier 1 Beard)" : 15, "Webber (Tier 1 Beard)" : 11.25,
-               "Woodie" : 45, "Were-Woodie" : 240, "Willow/Wes" : -30, "Other character" : 0}
+               "Woodie" : 45, "Were-Woodie" : 240, "Willow/Wes" : -30, "Other characters" : 0}
 layout = [
     [sg.Text('Choose your hat:')],
-    [sg.InputCombo(('Winter Hat', 'Beefalo Hat', "Tam o' Shanter", "Rabbit Earmuffs", 'Cat Cap', 'None'),key='hat_input', enable_events=True)],
+    [sg.InputCombo(('Winter Hat', 'Beefalo Hat', "Tam o' Shanter", "Rabbit Earmuffs", 'Cat Cap', 'None'),key='hat_input', default_value="Winter Hat", enable_events=True)],
     [sg.Text('Choose your dress:')],
-    [sg.InputCombo(('Dapper Vest', 'Hibernation Vest', 'Puffy Vest', 'Breezy Vest', 'Rain Coat', 'None'),key='dress_input', enable_events=True)],
+    [sg.InputCombo(('Dapper Vest', 'Hibernation Vest', 'Puffy Vest', 'Breezy Vest', 'Rain Coat', 'None'),key='dress_input', default_value="None", enable_events=True)],
     [sg.Text('Choose your character:')],
-    [sg.InputCombo(('Were-Woodie', 'Wilson (Tier 3 Beard)', 'Webber (Tier 3 Beard)', 'Wilson (Tier 2 Beard)', 'Webber (Tier 2 Beard)', 'Woodie', 'Wilson (Tier 1)', "Webber (Tier 1)", "Willow/Wes", 'Other character'), size=(20, 1), key='beard_input')],
+    [sg.InputCombo(('Were-Woodie', 'Wilson (Tier 3 Beard)', 'Webber (Tier 3 Beard)', 'Wilson (Tier 2 Beard)',
+                    'Webber (Tier 2 Beard)', 'Woodie', 'Wilson (Tier 1)', "Webber (Tier 1)", "Willow/Wes",
+                    'Other characters'), default_value="Other characters", size=(20, 1), key='beard_input',
+                   enable_events=True)],
     [sg.Text("Write your temperature:")],
-    [sg.Input(key='degrees_input')],
+    [sg.Input(key='degrees_input', default_text="50")],
     [sg.Text(size=(60,1), key='output0')],
     [sg.Text(size=(60,1), key='output1')],
     [sg.Text(size=(60,1), key='output2')],
@@ -25,14 +28,19 @@ window = sg.Window('DST Insulation Calculator', layout)
 
 while True:
     event, values = window.read()
+    if values['beard_input'] == "Were-Woodie":
+        window['hat_input'].update(disabled=True, value="None")
+        window['dress_input'].update(disabled=True, value="None")
+        values['hat_input'] = "None"
+        values['dress_input'] = "None"
+    if values['beard_input'] != "Were-Woodie":
+        window['hat_input'].update(disabled=False)
+        window['dress_input'].update(disabled=False)
     # See if user wants to quit or window was closed
     if event == sg.WINDOW_CLOSED or event == 'Exit':
         break
     if event == 'Calculate':
-        if values["beard_input"] == "Were-Woodie":
-            insulation_total = beard_dict["Were-Woodie"]
-        else:
-            insulation_total = hat_dict[values["hat_input"]] + dress_dict[values["dress_input"]] + beard_dict[values["beard_input"]] + 30
+        insulation_total = hat_dict[values["hat_input"]] + dress_dict[values["dress_input"]] + beard_dict[values["beard_input"]] + 30
         window['output0'].update('You will have {0} insulation.'.format(insulation_total))
         window['output1'].update('You will lose {0} degrees per second.'.format(30/insulation_total))
         window['output2'].update('You will start freezing after {0} seconds.'.format(int(values["degrees_input"])/(30/insulation_total)))
