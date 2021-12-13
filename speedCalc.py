feae6ea739346a4af0655155c18c8a20e3e6fa92
BrianEllingsgaard <72316548+BrianEllingsgaard@users.noreply.github.com>
2021-12-13 19:34:52 +0000

    insane update. i pretty much made everything cooler
    i still think it can be improved tho, esspecially my non neat typing

--- speedCalc.py
+++ speedCalc.py
@@ -2,50 +2,33 @@
 import PySimpleGUI as sg
 import pyperclip
 
-speed = ""
 
 # Character states:
 charStates_dict = {"Player":6, "Ghost":3.6, "Default beefalo":7 ,"Ornery beefalo":7, "Rider beefalo":8, "Pudgy beefalo":6.5}
 
-# some if statement to make sure you dont pick beef setting that i'm planning to add later.
 charMult_dict = {"Other characters" : 1, "WX-78 overcharge" : 1.5, "Wormwood bloom" : 1.2 ,"Woodie beaver" : 1.1, "Woodie goose" : 1.4, "Woodie moose" : 0.9}
 
 # Items
 handItems_dict = {"Empty":1,"Thulicite club" : 1.1, "Walking cane" : 1.25, "Lazy explorer" : 1.25}
 headItems_dict = {"Empty":1, "Ice cube":0.9}
-chestItems_dict = {"Empty":1, "Marble armor" : 0.7, "Piggy back" : 0.9, "Magiluminecense" : 1.2}
+chestItems_dict = {"Empty":1, "Marble armor" : 0.7, "Piggy back" : 0.9, "Magiluminecense" : 1.2, "Sculpture":0.15}
 
 exoticMults_dict = {"stormCheck":0.4,"roadCheck" : 1.3, "webbingCheck" : 0.6, "AntlionCheck" : 0.3, "honeyCheck" : 0.4}
 
 saddles_dict = {"Default":1.4, "Glossomor":1.55, "WarSaddle":1.25}
-    
+
+
 # Layout
 
+charStates_names = list(charStates_dict) # This is to get the keys
+# vals = list(exoticMults_dict.values()) # This is to get the values
 
-def DictToList(dictionary, list):
-    for a in dictionary.keys():
-        list.append(a)
-
-charStates_names = []
-DictToList(charStates_dict,charStates_names)
-
-charMult_names = []
-DictToList(charMult_dict,charMult_names)
-
-headItems_names = []
-DictToList(headItems_dict,headItems_names)
-
-chestItems_names = []
-DictToList(chestItems_dict,chestItems_names)
-
-handItems_names = []
-DictToList(handItems_dict,handItems_names)
-
-saddles_names = []
-DictToList(saddles_dict, saddles_names)
-
-exoticMults_names = []
-DictToList(exoticMults_dict,exoticMults_names)
+charMult_names = list(charMult_dict)
+headItems_names = list(headItems_dict)
+chestItems_names = list(chestItems_dict)
+handItems_names = list(handItems_dict)
+saddles_names  = list(saddles_dict)
+exoticMults_names = list(exoticMults_dict)
 
 layout = [
     # Character state : in other words, dead, alive, or is a beefalo
@@ -71,7 +54,7 @@ layout = [
     # Saddle
 
     [sg.Text("Choose your saddle")],
-    [sg.InputCombo(saddles_names, key="saddlesInput", default_value="Default", enable_events=True, disabled=True)],
+    [sg.InputCombo(saddles_names, key="saddlesInput", default_value="Default", enable_events=True, disabled=True, visible=True)],
 
 
     # Checkboxes
@@ -97,47 +80,67 @@ while True:
     if event == sg.WINDOW_CLOSED or event == 'Exit':
         break
 
-    if values['stateInput'] == "Player":
-        window['charInput'].update(disabled=False)
-        window['headInput'].update(disabled=False)
-        window['chestInput'].update(disabled=False)
+    if values["stateInput"] == "Player":
+        window["charInput"].update(disabled=False)
+        window["headInput"].update(disabled=False)
+        window["chestInput"].update(disabled=False)
         window["handInput"].update(disabled=False)
-        window['headInput'].update(disabled=False)
-        window['saddlesInput'].update(disabled=True)
+        window["headInput"].update(disabled=False)
+        window["saddlesInput"].update(disabled=True)
+             
+        if values["chestInput"] == "Sculpture":
+            window["charInput"].update(disabled=False)
+            window["headInput"].update(disabled=True)
+            window["chestInput"].update(disabled=False)
+            window["handInput"].update(disabled=True)
+            window["headInput"].update(disabled=True)
+            window["saddlesInput"].update(disabled=True)
 
-    elif values['stateInput'] == "Default beefalo" or values['stateInput'] == "Ornery beefalo" or values['stateInput'] == "Rider beefalo" or values['stateInput'] == "Pudgy beefalo":
-        window['charInput'].update(disabled=True)
-        window['headInput'].update(disabled=True)
-        window['chestInput'].update(disabled=True)
+    elif values["stateInput"] == "Default beefalo" or values['stateInput'] == "Ornery beefalo" or values['stateInput'] == "Rider beefalo" or values['stateInput'] == "Pudgy beefalo":
+        window["charInput"].update(disabled=True)
+        window["headInput"].update(disabled=True)
+        window["chestInput"].update(disabled=True)
         window["handInput"].update(disabled=True)
-        window['headInput'].update(disabled=True)
-        window['saddlesInput'].update(disabled=False)
+        window["headInput"].update(disabled=True)
+        window["saddlesInput"].update(disabled=False)
     
     elif values["stateInput"] == "Ghost":
-        window['charInput'].update(disabled=True)
-        window['headInput'].update(disabled=True)
-        window['chestInput'].update(disabled=True)
+        window["charInput"].update(disabled=True)
+        window["headInput"].update(disabled=True)
+        window["chestInput"].update(disabled=True)
         window["handInput"].update(disabled=True)
-        window['headInput'].update(disabled=True)
-        window['saddlesInput'].update(disabled=True)
+        window["headInput"].update(disabled=True)
+        window["saddlesInput"].update(disabled=True)
+
+    form = values['charInput']
+    if form == "Woodie beaver" or form == "Woodie goose" or form == "Woodie moose":
+        window["charInput"].update(disabled=False)
+        window["headInput"].update(disabled=True)
+        window["chestInput"].update(disabled=True)
+        window["handInput"].update(disabled=True)
+        window["headInput"].update(disabled=True)
+        window["saddlesInput"].update(disabled=True)
 
 
     if event == 'Calculate':
-
-        # TODO: beefalo saddles.
-
+        
+        chest = chestItems_dict[values['chestInput']]
+        character = charMult_dict[values["charInput"]]
+        head = headItems_dict[values['headInput']]
         state = charStates_dict[values["stateInput"]]
-        if values['stateInput'] == "Player":
+        hand = handItems_dict[values['handInput']]
 
-            character = charMult_dict[values["charInput"]]
-            head = headItems_dict[values['headInput']]
-            chest = chestItems_dict[values['chestInput']]
-            hand = handItems_dict[values['handInput']]
-            speed = state * character * head * chest * hand
+        if values['stateInput'] == "Player":
+            if form == "Woodie beaver" or form == "Woodie goose" or form == "Woodie moose":
+                speed = state * charMult_dict[values["charInput"]]
+            elif values['chestInput'] == "Sculpture":
+                speed = state*character*chest
+            else: 
+                speed = state * character * head * chest * hand
 
         elif values['stateInput'] == "Default beefalo" or values['stateInput'] == "Ornery beefalo" or values['stateInput'] == "Rider beefalo" or values['stateInput'] == "Pudgy beefalo": #"Player":6, "Ghost":3.6, "Default beefalo":7 ,"Ornery beefalo":7, "Rider beefalo":8, "Pudgy beefalo":6.5
             
-            speed = saddles_dict[values["saddlesInput"]]*charStates_dict[values["stateInput"]]
+            speed = saddles_dict[values["saddlesInput"]]*state
 
         elif values["stateInput"] == "Ghost":
             speed = charStates_dict[values["stateInput"]]
@@ -154,5 +157,4 @@ while True:
         pyperclip.copy('{0}'.format(speed))
 
 
-# Finish up by removing from the screen
 window.close()
